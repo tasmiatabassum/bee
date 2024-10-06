34e5ec91a2b363f0d366d60af3ca2be987a21915
Tasmia Tabassum <70054690+tasmiatabassum@users.noreply.github.com>
2024-10-06 16:54:33 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from datetime import datetime
 
 app = Flask(__name__)
@@ -7,6 +7,7 @@ app = Flask(__name__)
 start_date = datetime(2023, 10, 7, 4, 30)  
 
 @app.route('/')
+
 def home():
     now = datetime.now()
     time_together = now - start_date
@@ -20,21 +21,23 @@ def home():
         "everyday i wake up and thank the universe for sending you to me.",
         "im so grateful to have you here, with me.",
         "and im more grateful that i get to be with you, watch you grow and shine ✨",
-        "i know i can be a lot at times" , "mostly unreasonable and selfish :((", "but thank you so much for being the absolute best and putting up with my madness <33",
+        "i know i can be a lot at times, mostly unreasonable and selfish :(", "but thank you so much for being the absolute best and putting up with my madness",
         "i know love exists because you exist <33",
-        "can't wait to stare at you and not say a word <3 because i'm all words no game hehe ✨",
-        "i hope i light up your world like you do mine <33", "im sorry for being difficult sometimes :((", "i love you with all my heart and being", "like seriously, it hurts", 
-        "i love you, if i didn't say that already <33"
+        "can't wait to look at you and not say a word <3 beacuse i'm all words no game hehe ✨"
     ]
     
 
     spotify_links = [
+        {"name": "words", "url": url_for('full_circle')},
         {"name": "pov: im in love with you", "url": "https://open.spotify.com/playlist/5P3f6LDLtiV1JdHyi5D6qI?si=2efb90e312154955&pt=65792102ea70da88d5103239375651b1"}, 
         {"name": "full circle", "url": "https://open.spotify.com/playlist/1H9pnWTGDAWpUtPr8xvz7p?si=0b914e86a8b54897&pt=bd0e761c5c3ab6f22d0966252787de0f"} 
     ]
     pinterest_link = "https://www.pinterest.com/ttasmiatabassum/a-year-with-you/" 
 
     return render_template('index.html', days_together=days_together,hours_together=hours_together, minutes_together=minutes_together , messages=messages, spotify_links=spotify_links, pinterest_link=pinterest_link)
+@app.route('/words')
+def full_circle():
+    return render_template('full.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
